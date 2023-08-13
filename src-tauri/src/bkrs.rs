664c40ec4c8a9677b1b4570d3e3fb807cc3b1930
FedirM <tedmoster@gmail.com>
2023-08-13 05:19:17 +0300

    Test

---
+++ src-tauri/src/bkrs.rs
@@ -0,0 +1,55 @@
+
+
+pub mod BKRS {
+    use serde::{Deserialize, Serialize};
+    use std::collections::HashMap;
+    use std::error::Error;
+    use std::fs::read_to_string;
+
+    #[derive(Debug, Deserialize, Serialize, Clone)]
+    pub struct Nest {
+        pub ex: Option<String>,
+        pub pin: String,
+        pub ru: String
+    }
+
+    pub fn read_dic(file: &str) -> Result<HashMap<String, Option<Nest>>, Box<dyn Error>> {
+        let file_content = read_to_string(file)?;
+        let hash = serde_json::from_str(&file_content)?;
+        Ok(hash)
+    }
+
+    pub fn voca_list(dictionary: &HashMap<String, Option<Nest>>, chunk: &str) -> HashMap<String, Nest> {
+        let mut res = HashMap::new();
+        let buff: Vec<char> = chunk.chars().collect();
+        for (i, c) in buff.iter().enumerate() {
+            // Single check
+            if !res.contains_key(&c.to_string()) && dictionary.contains_key(&c.to_string()) {
+                if let Some(option) = dictionary.get(&c.to_string()) {
+                    match option {
+                        Some(nest) => {
+                            res.insert(c.to_string(), nest.clone());
+                        },
+                        None => {}
+                    }
+                }
+            }
+            // Multi check
+            let mut curr_str = String::from(c.clone());
+            for cc in buff.iter().skip(1 + i) {
+                curr_str.push(cc.clone());
+                if !res.contains_key(&curr_str) && dictionary.contains_key(&curr_str) {
+                    if let Some(option) = dictionary.get(&curr_str) {
+                        match option {
+                            Some(nest) => {
+                                res.insert(curr_str.clone(), nest.clone());
+                            },
+                            None => {}
+                        }
+                    }
+                }
+            }
+        }
+        res
+    }
+}
---
+++ src-tauri/src/main.rs
@@ -0,0 +1,143 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::{State, Manager, Size, LogicalSize};
+use urlencoding::encode;
+use std::sync::{Arc, Mutex};
+use std::collections::HashMap;
+
+mod bkrs;
+use bkrs::BKRS;
+
+const BKRS_VOCA_PATH: &str = "data/bkrs.json";
+// const BKRS_FORMS_PATH: &str = "data/bkrsForms.json";
+
+const GT_HI: &str = "tr_to_si";
+const GT_UA: &str = "tr_to_ua";
+const GT_VOCA: &str = "tr_voca";
+
+
+struct Store {
+    bkrs_voca: Arc<Mutex<HashMap<String, Option<BKRS::Nest>>>>,
+}
+
+impl Default for Store {
+    fn default() -> Self {
+        Store {
+            bkrs_voca: Arc::new(Mutex::new(BKRS::read_dic(BKRS_VOCA_PATH).unwrap())),
+        }
+    }
+}
+
+struct Storage {
+    store: Store
+}
+
+#[tauri::command]
+async fn vocabulary(chunks: Vec<String>, storage: State<'_, Storage>) -> Result<HashMap<String, BKRS::Nest>, ()> {
+    let store = &storage.store;
+    let dictionary = store.bkrs_voca.lock().unwrap();
+    let mut hash = HashMap::new();
+
+    for chunk in chunks {
+        hash.extend(BKRS::voca_list(&dictionary, &chunk))
+    }
+    Ok(hash)
+}
+
+#[tauri::command]
+async fn close_all_text_win(app: tauri::AppHandle) {
+    let wins = app.windows();
+    let v = vec![GT_HI, GT_UA, GT_VOCA];
+    for w in wins {
+        if v.iter().any(|s| w.0.eq(s)) {
+            w.1.close().unwrap();
+        }
+    }
+}
+
+#[tauri::command]
+async fn google_translator_ua(text: String, app: tauri::AppHandle) {
+    let window = tauri::WindowBuilder::new(
+        &app,
+        GT_UA,
+        tauri::WindowUrl::External(format!("https://translate.google.com.ua/?hl=ru&sl=zh-TW&tl=uk&text={}&op=translate", encode(&text)).parse().unwrap())
+    )
+    .initialization_script(INIT_SCRIPT_HI)
+    .build()
+    .unwrap();
+
+    window.set_size(Size::Logical(LogicalSize { width: 1280.0, height: 800.0 })).unwrap();
+}
+
+#[tauri::command]
+async fn google_translator_hi(text: String, app: tauri::AppHandle) {
+    let window = tauri::WindowBuilder::new(
+        &app,
+        GT_HI,
+        tauri::WindowUrl::External(format!("https://translate.google.com.ua/?hl=ru&sl=zh-TW&tl=zh-CN&text={}&op=translate", encode(&text)).parse().unwrap())
+    )
+    .initialization_script(INIT_SCRIPT_HI)
+    .build()
+    .unwrap();
+
+    window.set_size(Size::Logical(LogicalSize { width: 1280.0, height: 800.0 })).unwrap();
+}
+
+#[tauri::command]
+async fn google_voca(list: String, app: tauri::AppHandle) {
+    let window = tauri::WindowBuilder::new(
+        &app,
+        GT_VOCA,
+        tauri::WindowUrl::External(format!("https://translate.google.com.ua/?hl=ru&sl=zh-TW&tl=uk&text={}&op=translate", encode(&list)).parse().unwrap())
+    )
+    .build()
+    .unwrap();
+
+    window.set_size(Size::Logical(LogicalSize { width: 1280.0, height: 800.0 })).unwrap();
+}
+
+#[tauri::command]
+async fn google_voca_close(app: tauri::AppHandle) {
+    match app.get_window(GT_VOCA) {
+        Some(win) => {
+            match win.is_closable() {
+                Ok(_) => { win.close().unwrap() },
+                Err(_) => {}
+            }
+        },
+        None => {}
+    }
+}
+
+
+
+fn main() {
+    tauri::Builder::default()
+        .manage(Storage { store: Default::default() })
+        .invoke_handler(tauri::generate_handler![
+            google_translator_hi,
+            google_translator_ua,
+            close_all_text_win,
+            google_voca_close,
+            google_voca,
+            vocabulary
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+
+const INIT_SCRIPT_HI: &str = r#"
+    const interval = setInterval(() => {
+        const spans = document.getElementsByTagName('span');
+        console.log(spans.length);
+        if(spans.length > 400) {
+            for(let i = 0; i < spans.length; i++){
+                console.log(`span #${i}: ${spans[i].innerText}`);
+            }
+            clearInterval(interval);
+            interval = null;
+        }
+    }, 600);
+"#;
